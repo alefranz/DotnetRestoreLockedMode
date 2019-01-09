64ced3ac395d74b681e19a4ed9cf84c038f8c634
Alessio Franceschelli <alessio@franceschelli.me>
2019-01-09 18:33:00 +0000

    Initial version

---
+++ NetSdk22/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetSdk22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
